5082fab0ab538850044cd178db49ba59d6b44763
Kaori <jr.champ@gmail.com>
2024-01-20 18:49:57 +0900

    Update CSaveDataUnit.js 対人データを旧データから新データにアジャストさせる対応

--- ro4/m/js/CSaveDataUnit.js
+++ ro4/m/js/CSaveDataUnit.js
@@ -5164,10 +5164,10 @@ const SAVE_DATA_UNIT_TYPE_MOB_CONF_PLAYER = CSaveDataUnitTypeManager.register(
 				CSaveDataConst.propNameStWis,
 				CSaveDataConst.propNameStSplSign,
 				CSaveDataConst.propNameStSpl,
-				CSaveDataConst.propNameStConSign,
+/*				CSaveDataConst.propNameStConSign,
 				CSaveDataConst.propNameStCon,
 				CSaveDataConst.propNameStCrtSign,
-				CSaveDataConst.propNameStCrt,
+				CSaveDataConst.propNameStCrt,*/
 			];
 		}
 
@@ -5189,7 +5189,7 @@ const SAVE_DATA_UNIT_TYPE_MOB_CONF_PLAYER = CSaveDataUnitTypeManager.register(
 			// プロパティ定義情報の登録
 			this.registerPropInfo(CSaveDataConst.propNameOptCode, 6);
 			// （旧形式の領域的には52個用意されていたが末尾は使用していなかったので切り捨て）
-			this.registerPropInfo(CSaveDataConst.propNameParseCtrlFlag, 82);
+			this.registerPropInfo(CSaveDataConst.propNameParseCtrlFlag, 78);
 
 			this.registerPropInfo(CSaveDataConst.propNameStMaxHP, CSaveDataConst.propBitsMaxAbs4M);
 			this.registerPropInfo(CSaveDataConst.propNameStDefDiv, CSaveDataConst.propBitsMaxAbs10k);
@@ -5269,10 +5269,10 @@ const SAVE_DATA_UNIT_TYPE_MOB_CONF_PLAYER = CSaveDataUnitTypeManager.register(
 			this.registerPropInfo(CSaveDataConst.propNameStWis, CSaveDataConst.propBitsMaxAbs1k);
 			this.registerPropInfo(CSaveDataConst.propNameStSplSign, 1);
 			this.registerPropInfo(CSaveDataConst.propNameStSpl, CSaveDataConst.propBitsMaxAbs1k);
-			this.registerPropInfo(CSaveDataConst.propNameStConSign, 1);
+/*			this.registerPropInfo(CSaveDataConst.propNameStConSign, 1);
 			this.registerPropInfo(CSaveDataConst.propNameStCon, CSaveDataConst.propBitsMaxAbs1k);
 			this.registerPropInfo(CSaveDataConst.propNameStCrtSign, 1);
-			this.registerPropInfo(CSaveDataConst.propNameStCrt, CSaveDataConst.propBitsMaxAbs1k);
+			this.registerPropInfo(CSaveDataConst.propNameStCrt, CSaveDataConst.propBitsMaxAbs1k);*/
 		}
 
 
@@ -6653,7 +6653,7 @@ class CSaveDataUnitParse extends CSaveDataUnitBase {
 			saveDataUnit = new (CSaveDataUnitTypeManager.getUnitClass(SAVE_DATA_UNIT_TYPE_MOB_CONF_PLAYER))();
 			[dataTextWork, bitOffset] = saveDataUnit.convertFromOldFormat(dataTextWork, bitOffset,
 				0,
-				((1n << 82n) - 1n),
+				((1n << 78n) - 1n),
 
 				signValueArray[0][1],
 				signValueArray[1][1],
@@ -6678,7 +6678,7 @@ class CSaveDataUnitParse extends CSaveDataUnitBase {
 				signValueArray[41][1],
 				...(Array(1).fill(0)),
 				signValueArray[42][1],
-				...(Array(12).fill(0)),
+				...(Array(8).fill(0)),
 			);
 		}
 
