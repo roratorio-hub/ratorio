c032aee29859c7882d9ac7d77ec72a7ce41699ae
rag769 <96600388+rag769@users.noreply.github.com>
2024-03-05 12:03:43 +0900

    Support equip font color

--- ro4/m/js/saveimage.js
+++ ro4/m/js/saveimage.js
@@ -1,4 +1,46 @@
+// sample
+// https://ragnarokonline.gungho.jp/campaign_event/campaign/baselv220cp-2.html#modal
+// calcx.html?cx1cy1EtMmfo4Owqof.3M4X00cz11003-jkGlEgXaEjrmlefu0cz120032jAp3Vzgvlcz13.4fYl3cz14.4hj1cz15003Ydw7r8m4Inp6cw8cz16003OfJC0xmRjtn5Nacz170032j8nn3dksScz18.4fIm3cz19.32dhop8cz1a003_hcquumkYt6nsQmuaG0cz1b.4hM1cz1c.4hOacA1Z_1127456b89a3cA128c0cB1.sf_V___51d171n5n5nll5dldldl511cC1.ecR1.4S8cU1.cg003cW100Bcl3cZ121
 $(function () {
+  const v = (selector) => {
+    return $(selector).val() || $(selector).text();
+  }
+  const t = (selector) => {
+    return $(selector).text() || $(selector).val();
+  }
+  const e = (selector) => {
+    if (v(selector) == "0") {
+      return "-";
+    }
+    return t(selector);
+  }
+  const ench_count = (selector) => {
+    count = 0;
+    for (i = 1; i < 5; i++) {
+      if (i==1){
+        if (CARD_KIND_ENCHANT == CardObjNew[v(`${selector}_CARD_${i}`)][1]) {
+          count++;
+        }
+      } else if (v(`${selector}_CARD_${i}`) != "0"){
+        count++;
+      }
+    }
+    return count
+  }
+  const equip = (selector) => {
+    text = "";
+    if (v(selector + "_REFINE") != 0) {
+      text += `+${v(selector + "_REFINE")} `;
+    }
+    text +=
+      `${t(selector + " option:selected")} ( ${e(selector + "_CARD_1 option:selected") || "-"
+      } , ${e(selector + "_CARD_2 option:selected") || "-"
+      } , ${e(selector + "_CARD_3 option:selected") || "-"
+      } , ${e(selector + "_CARD_4 option:selected") || "-"
+      } )`;
+    return text;
+  }
+
   $("#save_image").click(function () {
     tpl = `
     <style>
@@ -11,57 +53,57 @@ $(function () {
       font-size: 12px;
       font-family: sans-serif;
     }
-    
+
     #imgdiv div#imgframe {
       background-image: url(../../ro4/m/img/frame.png);
       width: 1030;
       height: 720;
     }
-    
+
     #imgdiv div#base {
       position: relative;
       top: 60;
       left: 40;
     }
-    
+
     #imgdiv div#hp {
       position: relative;
       top: 70;
       left: 40;
     }
-    
+
     #imgdiv div#sp {
       position: relative;
       top: 80px;
       left: 40;
     }
-    
+
     #imgdiv div#status {
       position: relative;
       top: 150;
       left: 30;
       width: 290;
     }
-    
+
     #imgdiv table.status {
       width: 100%;
       border-collapse: collapse;
       border: 1px solid gray;
     }
-    
+
     #imgdiv table.status thead {
       background-color: rgb(189, 206, 247);
     }
-    
+
     #imgdiv table.status tr {
       border: 1px solid gray;
     }
-    
+
     #imgdiv table.status th,
     #imgdiv table.status td {
       padding: 3px 0px 3px 6px;
     }
-    
+
     #imgdiv table.status th {
       font-weight: bold;
       text-align: left;
@@ -69,21 +111,45 @@ $(function () {
       width: 20;
       font-size: 12px;
     }
-    
+
     #imgdiv table.status td {
       font-size: 11px;
     }
-    
+
     #imgdiv div#equip {
       position: relative;
       top: -365;
       left: 365;
       width: 620;
     }
-    
+
     #imgdiv dt {
       font-weight: bold;
       margin-top: 0.25rem;
+    }
+
+    #imgdiv dd {
+      margin-left: 2em;
+    }
+
+    #imgdiv dd.ench4 {
+      color: rgb(148, 0, 214);
+    }
+
+    #imgdiv dd.ench3 {
+      color: rgb(214, 132, 0);
+    }
+
+    #imgdiv dd.ench2 {
+      color: rgb(0, 107, 189);
+    }
+
+    #imgdiv dd.ench1 {
+      color: rgb(33, 90, 0);
+    }
+
+    #imgdiv dd.ench0 {
+      color: rgb(0, 0, 0);
     }
 
     #imgdiv div#cp {
@@ -93,22 +159,22 @@ $(function () {
     <div id="imgframe">
     <div id="base">
       <span>Base lv.</span>
-      <span>${$("#OBJID_SELECT_BASE_LEVEL").val()}</span>
+      <span>${v("#OBJID_SELECT_BASE_LEVEL")}</span>
       <span>/</span>
-      <span>${$("#OBJID_SELECT_JOB option:selected").text()}</span>
+      <span>${t("#OBJID_SELECT_JOB option:selected")}</span>
       <span>/</span>
       <span>Job lv.</span>
-      <span>${$("#OBJID_SELECT_JOB_LEVEL").val()}</span>
+      <span>${v("#OBJID_SELECT_JOB_LEVEL")}</span>
     </div>
     <div id="hp">
       <span>HP</span>
-      <span>${$("#OBJID_SPAN_CHARA_MAXHP").text()}</span>
+      <span>${t("#OBJID_SPAN_CHARA_MAXHP")}</span>
     </div>
     <div id="sp">
       <span>SP</span>
-      <span>${$("#OBJID_SPAN_CHARA_MAXSP").text()}</span>
+      <span>${t("#OBJID_SPAN_CHARA_MAXSP")}</span>
     </div>
-    
+
     <div id="status">
       <table class="status">
         <thead>
@@ -119,45 +185,45 @@ $(function () {
         <tbody>
           <tr>
             <th>Str</th>
-            <td>${$("#OBJID_SELECT_STATUS_STR").val()}${$("#OBJID_SPAN_STATUS_BONUS_STR").text()}</td>
+            <td>${v("#OBJID_SELECT_STATUS_STR")}${t("#OBJID_SPAN_STATUS_BONUS_STR")}</td>
             <th>Atk</th>
-            <td>${$("#OBJID_SPAN_CHARA_ATK").text()}</td>
+            <td>${t("#OBJID_SPAN_CHARA_ATK")}</td>
             <th>Def</th>
-            <td>${$("#OBJID_SPAN_CHARA_DEF").text()}</td>
+            <td>${t("#OBJID_SPAN_CHARA_DEF")}</td>
           </tr>
           <tr>
             <th>Agi</th>
-            <td>${$("#OBJID_SELECT_STATUS_AGI").val()}${$("#OBJID_SPAN_STATUS_BONUS_AGI").text()}</td>
+            <td>${v("#OBJID_SELECT_STATUS_AGI")}${t("#OBJID_SPAN_STATUS_BONUS_AGI")}</td>
             <th>Matk</th>
-            <td>${$("#OBJID_SPAN_CHARA_MATK").text()}</td>
+            <td>${t("#OBJID_SPAN_CHARA_MATK")}</td>
             <th>Mdef</th>
-            <td>${$("#OBJID_SPAN_CHARA_MDEF").text()}</td>
+            <td>${t("#OBJID_SPAN_CHARA_MDEF")}</td>
           </tr>
           <tr>
             <th>Vit</th>
-            <td>${$("#OBJID_SELECT_STATUS_VIT").val()}${$("#OBJID_SPAN_STATUS_BONUS_VIT").text()}</td>
+            <td>${v("#OBJID_SELECT_STATUS_VIT")}${t("#OBJID_SPAN_STATUS_BONUS_VIT")}</td>
             <th>Hit</th>
-            <td>${$("#OBJID_SPAN_CHARA_HIT").text()}</td>
+            <td>${t("#OBJID_SPAN_CHARA_HIT")}</td>
             <th>Flee</th>
-            <td>${$("#OBJID_SPAN_CHARA_FLEE").text()}</td>
+            <td>${t("#OBJID_SPAN_CHARA_FLEE")}</td>
           </tr>
           <tr>
             <th>Int</th>
-            <td>${$("#OBJID_SELECT_STATUS_INT").val()}${$("#OBJID_SPAN_STATUS_BONUS_INT").text()}</td>
+            <td>${v("#OBJID_SELECT_STATUS_INT")}${t("#OBJID_SPAN_STATUS_BONUS_INT")}</td>
             <th>Cri</th>
-            <td>${$("#OBJID_SPAN_CHARA_CRI").text()}</td>
+            <td>${t("#OBJID_SPAN_CHARA_CRI")}</td>
             <th>Aspd</th>
-            <td>${$("#OBJID_SPAN_CHARA_ASPD").text()}</td>
+            <td>${t("#OBJID_SPAN_CHARA_ASPD")}</td>
           </tr>
           <tr>
             <th>Dex</th>
-            <td>${$("#OBJID_SELECT_STATUS_DEX").val()}${$("#OBJID_SPAN_STATUS_BONUS_DEX").text()}</td>
+            <td>${v("#OBJID_SELECT_STATUS_DEX")}${t("#OBJID_SPAN_STATUS_BONUS_DEX")}</td>
             <th colspan="3">Status Point</th>
-            <td>${$("#A_STPOINT").text()}</td>
+            <td>${t("#A_STPOINT")}</td>
           </tr>
           <tr>
             <th>Luk</th>
-            <td>${$("#OBJID_SELECT_STATUS_LUK").val()}${$("#OBJID_SPAN_STATUS_BONUS_LUK").text()}</td>
+            <td>${v("#OBJID_SELECT_STATUS_LUK")}${t("#OBJID_SPAN_STATUS_BONUS_LUK")}</td>
             <th>Guild</th>
             <td colspan="3">ROラトリオHub</td>
           </tr>
@@ -173,51 +239,51 @@ $(function () {
         <tbody>
           <tr>
             <th>Pow</th>
-            <td>${$("#OBJID_SELECT_STATUS_POW").val()}${$("#OBJID_SPAN_STATUS_BONUS_POW").text()}</td>
+            <td>${v("#OBJID_SELECT_STATUS_POW")}${t("#OBJID_SPAN_STATUS_BONUS_POW")}</td>
             <th>P.Atk</th>
-            <td>${$("#OBJID_SPAN_STATUS_P_ATK").text()}</td>
+            <td>${t("#OBJID_SPAN_STATUS_P_ATK")}</td>
             <th>Res</th>
-            <td>${$("#OBJID_SPAN_STATUS_RES").text()}</td>
+            <td>${t("#OBJID_SPAN_STATUS_RES")}</td>
           </tr>
           <tr>
             <th>Sta</th>
-            <td>${$("#OBJID_SELECT_STATUS_STA").val()}${$("#OBJID_SPAN_STATUS_BONUS_STA").text()}</td>
+            <td>${v("#OBJID_SELECT_STATUS_STA")}${t("#OBJID_SPAN_STATUS_BONUS_STA")}</td>
             <th>S.Matk</th>
-            <td>${$("#OBJID_SPAN_STATUS_S_MATK").text()}</td>
+            <td>${t("#OBJID_SPAN_STATUS_S_MATK")}</td>
             <th>Mres</th>
-            <td>${$("#OBJID_SPAN_STATUS_MRES").text()}</td>
+            <td>${t("#OBJID_SPAN_STATUS_MRES")}</td>
           </tr>
           <tr>
             <th>Wis</th>
-            <td>${$("#OBJID_SELECT_STATUS_WIS").val()}${$("#OBJID_SPAN_STATUS_BONUS_WIS").text()}</td>
+            <td>${v("#OBJID_SELECT_STATUS_WIS")}${t("#OBJID_SPAN_STATUS_BONUS_WIS")}</td>
             <th>H.Plus</th>
-            <td>${$("#OBJID_SPAN_STATUS_H_PLUS").text()}</td>
+            <td>${t("#OBJID_SPAN_STATUS_H_PLUS")}</td>
             <th></th>
             <td></td>
           </tr>
           <tr>
             <th>Spl</th>
-            <td>${$("#OBJID_SELECT_STATUS_SPL").val()}${$("#OBJID_SPAN_STATUS_BONUS_SPL").text()}</td>
+            <td>${v("#OBJID_SELECT_STATUS_SPL")}${t("#OBJID_SPAN_STATUS_BONUS_SPL")}</td>
             <th>C.Rate</th>
-            <td>${$("#OBJID_SPAN_STATUS_C_RATE").text()}</td>
+            <td>${t("#OBJID_SPAN_STATUS_C_RATE")}</td>
             <th></th>
             <td></td>
           </tr>
           <tr>
             <th>Con</th>
-            <td>${$("#OBJID_SELECT_STATUS_CON").val()}${$("#OBJID_SPAN_STATUS_BONUS_CON").text()}</td>
+            <td>${v("#OBJID_SELECT_STATUS_CON")}${t("#OBJID_SPAN_STATUS_BONUS_CON")}</td>
             <th colspan="3">T.Status Point</th>
-            <td>${$("#OBJID_SPAN_STATUS_T_STATUS_POINT").text()}</td>
+            <td>${t("#OBJID_SPAN_STATUS_T_STATUS_POINT")}</td>
           </tr>
           <tr>
             <th>Crt</th>
-            <td>${$("#OBJID_SELECT_STATUS_CRT").val()}${$("#OBJID_SPAN_STATUS_BONUS_CRT").text()}</td>
+            <td>${v("#OBJID_SELECT_STATUS_CRT")}${t("#OBJID_SPAN_STATUS_BONUS_CRT")}</td>
             <td colspan="4"></td>
           </tr>
         </tbody>
       </table>
     </div>
-    
+
     <div id="equip">
       <div id="cp">
         <div>© Gravity Co., Ltd. & Lee MyoungJin(studio DTDS). All rights reserved.</div>
@@ -226,77 +292,25 @@ $(function () {
       <strong>== 装備一覧 ==</strong> (※ランダムオプションや補助装備を除く)
       <dl>
         <dt>【兜上段】</dt>
-        <dd>+ ${$("#OBJID_HEAD_TOP_REFINE").val()} ${$("#OBJID_HEAD_TOP option:selected").text()} ( ${$("#OBJID_HEAD_TOP_CARD_1 option:selected").text() || "-"
-        } , ${$("#OBJID_HEAD_TOP_CARD_2 option:selected").text() || "-"
-        } , ${$("#OBJID_HEAD_TOP_CARD_3 option:selected").text() || "-"
-        } , ${$("#OBJID_HEAD_TOP_CARD_4 option:selected").text() || "-"
-        } )
-        </dd>
+        <dd class="ench${ench_count("#OBJID_HEAD_TOP")}">${equip("#OBJID_HEAD_TOP")}</dd>
         <dt>【兜中段】</dt>
-        <dd>${$("#OBJID_HEAD_MID option:selected").text()} ( ${$("#OBJID_HEAD_MID_CARD_1 option:selected").text() || "-"
-        } , ${$("#OBJID_HEAD_MID_CARD_2 option:selected").text() || "-"
-        } , ${$("#OBJID_HEAD_MID_CARD_3 option:selected").text() || "-"
-        } , ${$("#OBJID_HEAD_MID_CARD_4 option:selected").text() || "-"
-        } )
-        </dd>
+        <dd class="ench${ench_count("#OBJID_HEAD_MID")}">${equip("#OBJID_HEAD_MID")}</dd>
         <dt>【兜下段】</dt>
-        <dd>${$("#OBJID_HEAD_UNDER option:selected").text()} ( ${$("#OBJID_HEAD_UNDER_CARD_1 option:selected").text() || "-"
-        } , ${$("#OBJID_HEAD_UNDER_CARD_2 option:selected").text() || "-"
-        } , ${$("#OBJID_HEAD_UNDER_CARD_3 option:selected").text() || "-"
-        } , ${$("#OBJID_HEAD_UNDER_CARD_4 option:selected").text() || "-"
-        } )
-        </dd>
+        <dd class="ench${ench_count("#OBJID_HEAD_UNDER")}">${equip("#OBJID_HEAD_UNDER")}</dd>
         <dt>【鎧】</dt>
-        <dd>+ ${$("#OBJID_BODY_REFINE").val()} ${$("#OBJID_BODY option:selected").text()} ( ${$("#OBJID_BODY_CARD_1 option:selected").text() || "-"
-        } , ${$("#OBJID_BODY_CARD_2 option:selected").text() || "-"
-        } , ${$("#OBJID_BODY_CARD_3 option:selected").text() || "-"
-        } , ${$("#OBJID_BODY_CARD_4 option:selected").text() || "-"
-        } )
-        </dd>
+        <dd class="ench${ench_count("#OBJID_BODY")}">${equip("#OBJID_BODY")}</dd>
         <dt>【右手】</dt>
-        <dd>+ ${$("#OBJID_ARMS_RIGHT_REFINE").val()} ${$("#OBJID_ARMS_RIGHT option:selected").text()} ( ${$("#OBJID_ARMS_RIGHT_CARD_1 option:selected").text() || "-"
-        } , ${$("#OBJID_ARMS_RIGHT_CARD_2 option:selected").text() || "-"
-        } , ${$("#OBJID_ARMS_RIGHT_CARD_3 option:selected").text() || "-"
-        } , ${$("#OBJID_ARMS_RIGHT_CARD_4 option:selected").text() || "-"
-        } )
-        </dd>
+        <dd class="ench${ench_count("#OBJID_ARMS_RIGHT")}">${equip("#OBJID_ARMS_RIGHT")}</dd>
         <dt>【左手】</dt>
-        <dd>+ ${$("#OBJID_ARMS_LEFT_REFINE").css("visibility") == "hidden" ? $("#OBJID_SHIELD_REFINE").val() : $("#OBJID_ARMS_LEFT_REFINE").val()
-        } ${$("#OBJID_ARMS_LEFT_REFINE").css("visibility") == "hidden" ? $("#OBJID_SHIELD option:selected").text() : $("#OBJID_ARMS_LEFT option:selected").text()
-        } ( ${($("#OBJID_ARMS_LEFT_REFINE").css("visibility") == "hidden" ? $("#OBJID_SHIELD_CARD_1 option:selected").text() : $("#OBJID_ARMS_LEFT_CARD_1 option:selected").text()) || "-"
-        } , ${($("#OBJID_ARMS_LEFT_REFINE").css("visibility") == "hidden" ? $("#OBJID_SHIELD_CARD_2 option:selected").text() : $("#OBJID_ARMS_LEFT_CARD_2 option:selected").text()) || "-"
-        } , ${($("#OBJID_ARMS_LEFT_REFINE").css("visibility") == "hidden" ? $("#OBJID_SHIELD_CARD_3 option:selected").text() : $("#OBJID_ARMS_LEFT_CARD_3 option:selected").text()) || "-"
-        } , ${($("#OBJID_ARMS_LEFT_REFINE").css("visibility") == "hidden" ? $("#OBJID_SHIELD_CARD_4 option:selected").text() : $("#OBJID_ARMS_LEFT_CARD_4 option:selected").text()) || "-"
-        } )
-        </dd>
+        <dd class="ench${ench_count($("#OBJID_ARMS_LEFT_REFINE").css("visibility") == "hidden" ? "#OBJID_SHIELD" : "#OBJID_ARMS_LEFT")}">${equip($("#OBJID_ARMS_LEFT_REFINE").css("visibility") == "hidden" ? "#OBJID_SHIELD" : "#OBJID_ARMS_LEFT")}</dd>
         <dt>【肩にかける物】</dt>
-        <dd>+ ${$("#OBJID_SHOULDER_REFINE").val()} ${$("#OBJID_SHOULDER option:selected").text()} ( ${$("#OBJID_SHOULDER_CARD_1 option:selected").text() || "-"
-        } , ${$("#OBJID_SHOULDER_CARD_2 option:selected").text() || "-"
-        } , ${$("#OBJID_SHOULDER_CARD_3 option:selected").text() || "-"
-        } , ${$("#OBJID_SHOULDER_CARD_4 option:selected").text() || "-"
-        } )
-        </dd>
+        <dd class="ench${ench_count("#OBJID_SHOULDER")}">${equip("#OBJID_SHOULDER")}</dd>
         <dt>【靴】</dt>
-        <dd>+ ${$("#OBJID_SHOES_REFINE").val()} ${$("#OBJID_SHOES option:selected").text()} ( ${$("#OBJID_SHOES_CARD_1 option:selected").text() || "-"
-        } , ${$("#OBJID_SHOES_CARD_2 option:selected").text() || "-"
-        } , ${$("#OBJID_SHOES_CARD_3 option:selected").text() || "-"
-        } , ${$("#OBJID_SHOES_CARD_4 option:selected").text() || "-"
-        } )
-        </dd>
+        <dd class="ench${ench_count("#OBJID_SHOES")}">${equip("#OBJID_SHOES")}</dd>
         <dt>【アクセサリー(1)】</dt>
-        <dd>${$("#OBJID_ACCESSARY_1 option:selected").text()} ( ${$("#OBJID_ACCESSARY_1_CARD_1 option:selected").text() || "-"
-        } , ${$("#OBJID_ACCESSARY_1_CARD_2 option:selected").text() || "-"
-        } , ${$("#OBJID_ACCESSARY_1_CARD_3 option:selected").text() || "-"
-        } , ${$("#OBJID_ACCESSARY_1_CARD_4 option:selected").text() || "-"
-        } )
-        </dd>
+        <dd class="ench${ench_count("#OBJID_ACCESSARY_1")}">${equip("#OBJID_ACCESSARY_1")}</dd>
         <dt>【アクセサリー(2)】</dt>
-        <dd>${$("#OBJID_ACCESSARY_2 option:selected").text()} ( ${$("#OBJID_ACCESSARY_2_CARD_1 option:selected").text() || "-"
-        } , ${$("#OBJID_ACCESSARY_2_CARD_2 option:selected").text() || "-"
-        } , ${$("#OBJID_ACCESSARY_2_CARD_3 option:selected").text() || "-"
-        } , ${$("#OBJID_ACCESSARY_2_CARD_4 option:selected").text() || "-"
-        } )
-        </dd>
+        <dd class="ench${ench_count("#OBJID_ACCESSARY_2")}">${equip("#OBJID_ACCESSARY_2")}</dd>
       </dl>
     </div>
     `;
@@ -307,11 +321,9 @@ $(function () {
     div.append(tpl);
     $(".content").append(div);
     dd = $("#equip dd");
-    for(i=0;i<dd.length;i++) {
-      $(dd[i]).text($(dd[i]).text().replace(/ *\(\+\d+以上\)/g,""))
-      $(dd[i]).text($(dd[i]).text().replace(/\(?(カードなし|エンチャントなし)\)?/g,"-"))
-      $(dd[i]).text($(dd[i]).text().replace(/【習】/g,""))
-      $(dd[i]).text($(dd[i]).text().replace("+ 0 ",""))
+    for (i = 0; i < dd.length; i++) {
+      $(dd[i]).text($(dd[i]).text().replace(/ *\(\+\d+以上\)/g, ""))
+      $(dd[i]).text($(dd[i]).text().replace(/【習】/g, ""))
     }
 
     html2canvas(document.querySelector("#imgdiv"), { allowTaint: true, useCORS: true }).then(
