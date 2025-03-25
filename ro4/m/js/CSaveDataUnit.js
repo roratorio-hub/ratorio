998bce6b1d19cd8452553412cd976d24bbcb5f3b
usachoco <149866018+usachoco@users.noreply.github.com>
2025-03-26 06:29:48 +0900

    コメント追記

--- ro4/m/js/CSaveDataUnit.js
+++ ro4/m/js/CSaveDataUnit.js
@@ -6226,6 +6226,7 @@ const SAVE_DATA_UNIT_TYPE_SETTINGS = CSaveDataUnitTypeManager.register(
 			// バージョニングできていない古いデータ形式の互換性確保
 			if (dataText.length < 7) {
 				let nextOffset = super.parse(dataText + "0", bitOffset);
+				// 古いバージョンで AttackAutoCalc として使われていたデータを、現行バージョンでは ConfirmDialogSwitch として扱う
 				const attach_auto_calc_old = this.parsedMap.get(CSaveDataConst.propNameConfirmDialogSwitch);
 				this.parsedMap.set(CSaveDataConst.propNameAttackAutoCalc, attach_auto_calc_old);
 				return nextOffset;
