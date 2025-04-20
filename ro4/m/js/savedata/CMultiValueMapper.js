022c983d2c128c3d3d6c728b7e4033917acb8220
usachoco <149866018+usachoco@users.noreply.github.com>
2025-04-21 06:14:39 +0900

    CSaveDataUnit関連クラスをディレクトリにまとめました

--- ro4/m/js/savedata/CMultiValueMapper.js
+++ ro4/m/js/savedata/CMultiValueMapper.js
@@ -4,15 +4,12 @@
  * （複数の値が設定された場合、値は配列でラッピングされて保持、取得される）
  */
 class CMultiValueMapper extends SKeyMap {
-
 	/**
 	 * コンストラクタ.
 	 */
 	constructor () {
 		super();
 	}
-
-
 
 	/**
 	 * 値を追加する.
@@ -22,29 +19,23 @@ class CMultiValueMapper extends SKeyMap {
 	 * @throws {Error} 指定のキーが既に登録済みの場合
 	 */
 	add (key, value) {
-
 		// すでに登録されている値を取得する
 		const valueNow = this.get(key);
-
 		// すでに登録されている値がある場合
 		if (valueNow !== undefined) {
-
 			// 配列化されている場合は、配列に追加する
 			if (Array.isArray(valueNow)) {
 				valueNow.push(value);
 			}
-
 			// 配列化されていない場合は、配列化して登録しなおす
 			else {
 				super.set(key, [valueNow, value]);
 			}
 		}
-
 		// まだ登録されていない場合は、そのまま登録する
 		else {
 			super.set(key, value);
 		}
-
 		return key;
 	}
 }
