162f6a5f688109b822b366626c3998788988f19a
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2025-09-08 22:46:21 +0900

    [add] Added save data opt-in feature.

--- workspace/bootstrap.js
+++ workspace/bootstrap.js
@@ -3,4 +3,5 @@ import './src/loadItemMap.ts';
 import './src/loadJobMap.ts';
 import './src/loadSkillMap.ts';
 import './src/rodbTranslator.ts';
+import './src/optInSavedata.ts';
 import './src/startup.ts';
--- workspace/src/common.d.ts
+++ workspace/src/common.d.ts
@@ -1,5 +1,8 @@
 declare function AutoCalc(): void;
 declare function CalcStatusPoint(flag: boolean): void;
 declare function OnClickSkillSWLearned(): void;
+declare function OnClickUrlOutMIG(): void;
 declare function StAllCalc(): void;
 declare function changeJobSettings(jobId: string): void;
+declare function generateImage(): void;
+declare function html2canvas(a: any, b: any): Promise<HTMLCanvasElement>;
