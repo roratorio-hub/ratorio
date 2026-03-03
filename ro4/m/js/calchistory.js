84376481f3a62091ba433060989ba3c9cdd654ec
usachoco <usachoco@users.noreply.github.com>
2026-03-03 13:34:37 +0000

    deploy: ca9b986c171654ad8e442b6823838d5e2f570401

--- ro4/m/js/calchistory.js
+++ ro4/m/js/calchistory.js
@@ -139,9 +139,10 @@ div.clip_memo {
           const canvasPosition = Chart.helpers.getRelativePosition(e, chart);
           const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
           if (chart.data.datasets[0].data.length > dataX) {
-            url = chart.data.datasets[0].metadata[Math.abs(dataX)]["url"];
+            let url = chart.data.datasets[0].metadata[Math.abs(dataX)]["url"];
             CSaveController.loadFromURL(url);
             CItemInfoManager.OnClickExtractSwitch();
+            LoadSelect2();
           }
         }
       }
