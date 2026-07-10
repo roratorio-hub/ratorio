670bfa0e7474378727fb5d23e5a529e0626f7d54
usachoco <149866018+usachoco@users.noreply.github.com>
2026-07-11 02:21:04 +0900

    四方五行陣の攻撃力を上方修正

--- roro/m/js/CSkillManager.js
+++ roro/m/js/CSkillManager.js
@@ -39854,7 +39854,7 @@ export function CSkillManager() {
 			}
 			this.Power = function(skillLv, charaData, option) {       // スキル倍率
 				let ratio = 0;
-				ratio = 2200 + 600 * skillLv;
+				ratio = 4280 + 600 * skillLv;
 				ratio += 5 * GetTotalSpecStatus(MIG_PARAM_ID_SPL);
 				const gofu_shuren_lv = Math.max(LearnedSkillSearch(SKILL_ID_GOFU_SHUREN), UsedSkillSearch(SKILL_ID_GOFU_SHUREN));
 				const reidozyutsu_shuren_lv = Math.max(LearnedSkillSearch(SKILL_ID_REIDOZYUTSU_SHUREN), UsedSkillSearch(SKILL_ID_REIDOZYUTSU_SHUREN));
